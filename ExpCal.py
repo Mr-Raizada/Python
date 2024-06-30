088c096d1fc20971ebdb51463a7a9533b0e5ce27
Vipul Singh <unhindered.passion@gmail.com>
2024-06-30 20:35:12 +0530

    My First Commit to learn Python

---
+++ ExpCal.py
@@ -0,0 +1,7 @@
+#Using the Python as a calculator 
+a = 2+2
+print(a)
+
+#Division always returns a floating point numbers 
+b = 21/8
+print(b)
---
+++ Expcomment.py
@@ -0,0 +1,6 @@
+#using comment 
+#!/path/to/interpreter 
+rohn = True
+if rohn : 
+    print("Hii Rony")
+    
